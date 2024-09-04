22703c5b0585f60e3ff2d231a54f642fb3b79e65
safhanalikhan <safhanalikhan@example.com>
2024-09-04 19:55:29 +0500

    without authentication

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -9,7 +9,7 @@ const bucketName = 'deepwallec2';
 
 export default async function handle(req, res) {
   await mongooseConnect();
-  await isAdminRequest(req, res);
+  // await isAdminRequest(req, res);
 
   const form = new multiparty.Form();
   const { files } = await new Promise((resolve, reject) => {
